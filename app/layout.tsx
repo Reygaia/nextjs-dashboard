1862449d53215f1616827bb9737fd221f0778ae7
Reygaia <mrcobanfrog@Gmail.com>
2024-07-17 01:43:51 +0700

    nextjs dashboard learning project commit to push to vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import "@/app/ui/global.css";
+import { inter, playwrite } from "@/app/ui/fonts";
+
 export default function RootLayout({
-  children,
+      children,
 }: {
-  children: React.ReactNode;
+      children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+      return (
+            <html lang="en">
+                  <body className={`${playwrite.className} antialiased`}>
+                        {children}
+                  </body>
+            </html>
+      );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+import { Playwrite_US_Modern } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const playwrite = Playwrite_US_Modern({ style:"normal" });
